ef79d24a614f2d9f661ed8bf2ecb1accc9c38a81
Ljubomir Reljin <Ljubomir Reljin>
2017-04-21 22:05:28 +0200

    Inicijalno podesavanje mape

---
+++ resources/views/front.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+
+@section('content')
+	
+	<div class="container">
+		<div class="col-md-6 col-md-offset-3" style="text-align: center">
+			<h2>Laramap</h2>
+			<br>
+		</div>
+	</div>
+	<div id="map">
+		
+	</div>
+
+@endsection
